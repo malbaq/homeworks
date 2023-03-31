c3a61dd4e1111fad9d8c4d4e55403c8658f746a3
foxstudent101135 <z5Hk9H6Zh6>
2023-03-31 14:10:17 -0400

    Done

--- CounterClass.py
+++ CounterClass.py
@@ -1,2 +1,26 @@
+class MaxValueReachedException(Exception):
+    pass
+
 class Counter:
-    ...
+    def __init__(self, start=0, stop=None) -> None:
+        self.start = start
+        self.stop = stop
+    
+    def increment(self):
+        if self.stop != None:
+            if self.start < self.stop:
+                self.start += 1
+            else:
+                raise MaxValueReachedException
+        else:
+            self.start += 1
+
+    def get(self):
+        return self.start
+
+try:
+    counter = Counter(4, 3)
+    counter.increment()
+    print(counter.get())
+except MaxValueReachedException:
+    print('Max value reached')
