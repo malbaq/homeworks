502a3f41efd7a21dcaffa577ddbd58b599f13fa2
foxstudent101135 <z5Hk9H6Zh6>
2023-03-31 18:38:17 -0400

    Task 6.2. Memorizing 10 last items of the dict

---
+++ Deque.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+
+class CustomDict:
+    def __init__(self):
+        self._data = {}
+        self._history = deque(maxlen=3)
+
+    def __setitem__(self, key, value):
+        self._data[key] = value
+        self._history.append(key)
+
+    def __getitem__(self, key):
+        return self._data[key]
+
+    def get_history(self):
+        return self._history
+
+my_dict = CustomDict()
+
+try:
+    while True:
+        new_key = input('Specify the key: ')
+        new_value = input('Add the value: ')
+        my_dict[new_key] = new_value
+except EOFError:
+    print('\n', list(my_dict.get_history()))
